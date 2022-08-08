a466496f380327ba12cedd71c6ea5c27eca57a1c
Elina <elina.melkonyan.y@tumo.org>
2022-08-08 19:22:55 +0400

    43

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        switch(year%4){
+            case 0:
+                System.out.println("leap");
+            case 1:
+                System.out.println("not leap");
+        }
     }
 }
